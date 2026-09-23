1a2b05bc5ab3f190c2dcbe58f04dc4b4b8e28f57
prixiesticks <aoibhegreaney264@gmail.com>
2026-09-23 13:17:00 +0000

    First Java File

---
+++ Welcome1.java
@@ -0,0 +1,17 @@
+public class Welcome1{
+    public static void main(String[] args) {
+        // all code here 
+        /* Name : Aoibhe 
+        Date : 21/09 
+        Desc: Example first java program */
+
+        System.out.println("First Java program");
+        System.out.println("Another line of output");
+        System.out.println("Same line");
+        System.out.println("Still same line");
+        System.out.println();            
+          
+
+
+    }
+}
